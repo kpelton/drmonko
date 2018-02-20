6f94b7279e1275322b50f0811d349e9a516cfd36
Kyle Pelton <peltonkyle@gmail.com>
2018-02-19 17:28:44 -0800

    Fix screen size

--- src/sdlWindow.h
+++ src/sdlWindow.h
@@ -12,8 +12,8 @@
 #include <time.h>
 
 
-#define SCREEN_WIDTH  1024
-#define SCREEN_HEIGHT 768
+#define SCREEN_WIDTH  1920
+#define SCREEN_HEIGHT 1080
 #define SCREEN_BPP     32
 
 class sdlWindow {
